e3d773c60f581cebfc81cde7f83b863df277780d
magix257 <55533437+magix257@users.noreply.github.com>
2020-01-20 11:59:46 +0100

    Initial commit

---
+++ src/main/java/pl/slawek/Controller.java
@@ -0,0 +1,14 @@
+package pl.slawek;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+	@RequestMapping("/")
+	public String index() {
+		
+		return "index.html";
+	}
+	
+}
